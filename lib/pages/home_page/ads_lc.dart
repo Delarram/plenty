6293da8ea9e2071cf39c5b0d45709e64c2cff2c9
Delarram <tunyanaing960@gmail.com>
2023-03-11 01:09:53 +0630

    added slider section on home

---
+++ lib/pages/home_page/ads_lc.dart
@@ -0,0 +1,101 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:newpj/widgets/custom_text.dart';
+import 'package:carousel_slider/carousel_slider.dart';
+import '../../resources/color_const.dart';
+
+
+class LocalServiceItem extends StatefulWidget {
+
+   LocalServiceItem({Key? key,})
+      : super(key: key);
+
+  List<String> localServiceList = [
+    "https://images.wallpapersden.com/image/download/purple-sunrise-4k-vaporwave_bGplZmiUmZqaraWkpJRmbmdlrWZlbWU.jpg",
+    "https://wallpaperaccess.com/full/2637581.jpg",
+    "https://uhdwallpapers.org/uploads/converted/20/01/14/the-mandalorian-5k-1920x1080_477555-mm-90.jpg"
+  ];
+
+  @override
+  _LocalServiceItemState createState() => _LocalServiceItemState();
+}
+
+class _LocalServiceItemState extends State<LocalServiceItem> {
+  int _current = 0;
+
+
+  @override
+  Widget build(BuildContext context) {
+    final List<Widget> imageSliders = widget.localServiceList
+        .map((item) => ClipRRect(
+      borderRadius: BorderRadius.circular(12),
+      child: GestureDetector(
+        onTap: (){
+        },
+        child: Container(
+          width: double.infinity,
+          height: double.infinity,
+          decoration: BoxDecoration(
+            image: DecorationImage(
+                image: CachedNetworkImageProvider(item.toString()),
+                fit: BoxFit.fill),
+          ),
+        ),
+      ),
+    ))
+        .toList();
+
+    return Column(
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            Padding(
+              padding: EdgeInsets.only(right: 10.w, top: 5.h),
+              child: CustomTextView(
+                    text: "All",
+                    fontColor: cSecondaryOne,
+                    fontSize: 15.sp,
+                    fontWeight: FontWeight.bold,
+                  ),
+            )
+          ],
+        ),
+        Expanded(
+          child: CarouselSlider(
+            items: imageSliders,
+            options: CarouselOptions(
+                autoPlay: true,
+                enlargeCenterPage: true,
+                aspectRatio: 3.5,
+                onPageChanged: (index, something) {
+                  setState(() {
+                    _current = index;
+                  });
+                }),
+          ),
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: widget.localServiceList.map((url) {
+            int index = widget.localServiceList.indexOf(url);
+            return Container(
+              width: 9.h,
+              height: 9.w,
+              margin: EdgeInsets.symmetric(
+                vertical: 15.h,
+                horizontal: 2.w,
+              ),
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                color: _current == index ? cPrimaryOne : cBackGroundFive,
+              ),
+            );
+          }).toList(),
+        ),
+      ],
+    );
+  }
+
+}
--- lib/pages/home_page/home_page.dart
+++ lib/pages/home_page/home_page.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-
 import 'package:cached_network_image/cached_network_image.dart';
+import 'package:newpj/pages/home_page/ads_lc.dart';
 import 'package:newpj/pages/home_page/home_page_listview.dart';
 import '../../resources/color_const.dart';
 
@@ -13,170 +13,81 @@ class Homepage extends StatefulWidget {
     Key? key,
   }) : super(key: key);
 
-
-
   @override
   State<Homepage> createState() => _HomepageState();
 }
 
 class _HomepageState extends State<Homepage> {
   @override
-
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor:cBackGroundFive,
+      backgroundColor: cBackGroundFive,
       body: Container(
-        decoration: BoxDecoration(
-        ),
+        decoration: BoxDecoration(color: Colors.white),
         child: NestedScrollView(
-          headerSliverBuilder:
-              (BuildContext context, bool innerBoxIsScrolled) {
-            return <Widget>[
-              SliverAppBar(
-                leading: Builder(builder: (BuildContext context) {
-                  return GestureDetector(
-                    onTap: () {
-                      Navigator.pop(context);
-                    },
-                    child: Container(
-                      margin: EdgeInsets.only(left: 10),
-                      height: 20,
-                      width: 20,
-                      decoration: BoxDecoration(
-                          shape: BoxShape.circle,
-                          color: Colors.white,
-                        border: Border.all(width: 3,color: cSecondaryOne)
+            headerSliverBuilder:
+                (BuildContext context, bool innerBoxIsScrolled) {
+              return <Widget>[
+                SliverAppBar(
+                  leading: Builder(builder: (BuildContext context) {
+                    return GestureDetector(
+                      onTap: () {
+                        Navigator.pop(context);
+                      },
+                      child: Container(
+                        margin: EdgeInsets.only(left: 10),
+                        height: 20,
+                        width: 20,
+                        decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            color: Colors.white,
+                            border: Border.all(width: 3, color: cSecondaryOne)),
                       ),
-                    ),
-                  );
-                }),
-                title: Builder(builder: (BuildContext context) {
-                  return CustomTextView(
-                    text: "This is the title",
-                    fontColor: cBackGroundOne,
-                    fontWeight: FontWeight.bold,
-                    fontSize: 18.sp,
-                  );
-                }),
-                iconTheme: const IconThemeData(
-                  color: Colors.black,
-                ),
-                backgroundColor: cBackGroundWhite,
-                expandedHeight: MediaQuery.of(context).size.height / 4,
-                floating: false,
-                pinned: true,
-                flexibleSpace: FlexibleSpaceBar(
-                  centerTitle: true,
-                  background: Container(
-                      decoration: const BoxDecoration(
-                      color: cBackGroundFive
-                      ),
+                    );
+                  }),
+                  title: Builder(builder: (BuildContext context) {
+                    return CustomTextView(
+                      text: "This is the title",
+                      fontColor: cBackGroundOne,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 18.sp,
+                    );
+                  }),
+                  iconTheme: const IconThemeData(
+                    color: Colors.black,
+                  ),
+                  backgroundColor: cBackGroundWhite,
+                  expandedHeight: MediaQuery.of(context).size.height / 4,
+                  floating: false,
+                  pinned: true,
+                  flexibleSpace: FlexibleSpaceBar(
+                    centerTitle: true,
+                    background: Container(
+                      decoration: const BoxDecoration(color: cBackGroundFive),
                       child: Column(
                         mainAxisAlignment: MainAxisAlignment.end,
                         children: [
-                          Flexible(
-                            child: CachedNetworkImage(
-                              height: 100.w,
-                              fit: BoxFit.cover,
-                              imageUrl:
-                              //widget.yogaListVO.videoImage ?? "",
-                              "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS5-F8otEAn1mYIv9mvLPu97JHBMG_9IWR_MVhxjktJ7SRe-BwAAxUC2I-HjgY-jGbwVSM&usqp=CAU",
-                              progressIndicatorBuilder:
-                                  (context, url, downloadProgress) =>
-                                  Center(
-                                    child: CircularProgressIndicator(
-                                        value: downloadProgress.progress),
-                                  ),
-                              errorWidget: (context, url, error) =>
-                              const Icon(Icons.error),
-                            ),
+                          SizedBox(
+                            height: 70,
                           ),
+                          Flexible(child: LocalServiceItem()),
                         ],
-                      )),
+                      ),
+                    ),
+                  ),
                 ),
-              ),
-            ];
-          },
-          body:Container(
-            height: 30,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.only(topRight: Radius.circular(20),topLeft: Radius.circular(20)),
-              color: Colors.white
-            ),
-          )
-        ),
+              ];
+            },
+            body: Container(
+              height: 30,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.only(
+                      topRight: Radius.circular(20),
+                      topLeft: Radius.circular(20)),
+                  color: Colors.white),
+            )),
       ),
     );
   }
 }
-
-// class StorageDialog extends StatelessWidget {
-//   const StorageDialog({
-//     Key? key,
-//
-//   }) : super(key: key);
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return Container(
-//       height: MediaQuery.of(context).size.height / 4.0,
-//       width: MediaQuery.of(context).size.width / 1.5,
-//       padding: EdgeInsets.symmetric(
-//           horizontal: 20.r,
-//           vertical: 12.r),
-//       child: Column(
-//         mainAxisAlignment: MainAxisAlignment.center,
-//         crossAxisAlignment: CrossAxisAlignment.center,
-//         children: [
-//           Padding(
-//             padding:
-//             EdgeInsets.symmetric(
-//                 horizontal: 20.r
-//             ),
-//             child:
-//             CustomTextView(
-//               text:
-//               "You need to enable storage permission to download video",
-//               fontSize: 16.sp,
-//               fontWeight: FontWeight.w500,
-//               textAlign:
-//               TextAlign.center,
-//             ),
-//           ),
-//           SizedBox(
-//             height: 12.h,
-//           ),
-//           Padding(
-//             padding:  EdgeInsets.symmetric(horizontal: 15.w),
-//             child: CustomButtonView(
-//                 height: 25.h,
-//                 borderRadius: 5.w,
-//                 text: "Open app setting",
-//                 buttonColor: cSecondaryOne,
-//                 fontColor: cBackGroundSix,
-//                 fontSize: 12.sp,
-//                 onPressed: () async{
-//                   await openAppSettings();
-//                 }),
-//           ),
-//           SizedBox(height: 5.w,),
-//           Padding(
-//             padding:  EdgeInsets.symmetric(horizontal: 15.w),
-//             child: CustomButtonView(
-//                 height: 25.h,
-//                 borderRadius: 5.w,
-//                 text: "Deny",
-//                 buttonColor: cSecondaryOne,
-//                 fontColor: cBackGroundSix,
-//                 fontSize: 12.sp,
-//                 onPressed: () {
-//                   Navigator.pop(context);
-//                 }),
-//           ),
-//         ],
-//       ),
-//     );
-//   }
-// }
