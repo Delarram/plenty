2c579788438d931802887f6a666112842258bb65
Delarram <tunyanaing960@gmail.com>
2023-03-11 00:24:20 +0630

    refactor page

--- lib/pages/home_page/home_page.dart
+++ lib/pages/home_page/home_page.dart
@@ -26,146 +26,86 @@ class _HomepageState extends State<Homepage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: NestedScrollView(
-        headerSliverBuilder:
-            (BuildContext context, bool innerBoxIsScrolled) {
-          return <Widget>[
-            SliverAppBar(
-              leading: Builder(builder: (BuildContext context) {
-                return GestureDetector(
-                  onTap: () {
-                    Navigator.pop(context);
-                  },
-                  child: Container(
-                    margin: EdgeInsets.only(left: 10),
-                    height: 20,
-                    width: 20,
-                    decoration: BoxDecoration(
-                        shape: BoxShape.circle,
-                        color: Colors.white,
-                      border: Border.all(width: 3,color: cSecondaryOne)
+      backgroundColor:cBackGroundFive,
+      body: Container(
+        decoration: BoxDecoration(
+        ),
+        child: NestedScrollView(
+          headerSliverBuilder:
+              (BuildContext context, bool innerBoxIsScrolled) {
+            return <Widget>[
+              SliverAppBar(
+                leading: Builder(builder: (BuildContext context) {
+                  return GestureDetector(
+                    onTap: () {
+                      Navigator.pop(context);
+                    },
+                    child: Container(
+                      margin: EdgeInsets.only(left: 10),
+                      height: 20,
+                      width: 20,
+                      decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: Colors.white,
+                        border: Border.all(width: 3,color: cSecondaryOne)
+                      ),
                     ),
-                  ),
-                );
-              }),
-              title: Builder(builder: (BuildContext context) {
-                return CustomTextView(
-                  text: "This is the title",
-                  fontColor: cBackGroundOne,
-                  fontWeight: FontWeight.bold,
-                  fontSize: 18.sp,
-                );
-              }),
-              iconTheme: const IconThemeData(
-                color: Colors.black,
-              ),
-              backgroundColor: cBackGroundWhite,
-              expandedHeight: MediaQuery.of(context).size.height / 4,
-              floating: false,
-              pinned: true,
-              flexibleSpace: FlexibleSpaceBar(
-                centerTitle: true,
-                background: Container(
-                    decoration: const BoxDecoration(
-                        gradient: LinearGradient(colors: [
-                          Color(0xffB0F1F3),
-                          Color(0xffF7E2FF),
-                        ])),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      children: [
-                        Flexible(
-                          child: CachedNetworkImage(
-                            height: 150.w,
-                            fit: BoxFit.cover,
-                            imageUrl:
-                            //widget.yogaListVO.videoImage ?? "",
-                            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS5-F8otEAn1mYIv9mvLPu97JHBMG_9IWR_MVhxjktJ7SRe-BwAAxUC2I-HjgY-jGbwVSM&usqp=CAU",
-                            progressIndicatorBuilder:
-                                (context, url, downloadProgress) =>
-                                Center(
-                                  child: CircularProgressIndicator(
-                                      value: downloadProgress.progress),
-                                ),
-                            errorWidget: (context, url, error) =>
-                            const Icon(Icons.error),
-                          ),
-                        ),
-                      ],
-                    )),
-              ),
-            ),
-          ];
-        },
-        body: Container(
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(12)
-          ),
-          child: Column(
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      CustomTextView(
-                        text: "Second Title",
-                        fontSize: 22.sp,
-                        fontWeight: FontWeight.w700,
+                  );
+                }),
+                title: Builder(builder: (BuildContext context) {
+                  return CustomTextView(
+                    text: "This is the title",
+                    fontColor: cBackGroundOne,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 18.sp,
+                  );
+                }),
+                iconTheme: const IconThemeData(
+                  color: Colors.black,
+                ),
+                backgroundColor: cBackGroundWhite,
+                expandedHeight: MediaQuery.of(context).size.height / 4,
+                floating: false,
+                pinned: true,
+                flexibleSpace: FlexibleSpaceBar(
+                  centerTitle: true,
+                  background: Container(
+                      decoration: const BoxDecoration(
+                      color: cBackGroundFive
                       ),
-                      SizedBox(
-                        height: 5.h,
-                      ),
-                      CustomTextView(
-                        text:
-                            "under one ",
-                        fontSize: 14.sp,
-                        fontWeight: FontWeight.w500,
-                        fontFamily: 'poppin',
-                        fontColor: cBackGroundThree,
-                      ),
-                    ],
-                  ),
-                  Container(
-                    height: 30.h,
-                    decoration: BoxDecoration(
-                        color: cSecondaryOne,
-                        borderRadius: BorderRadius.circular(30.r)),
-                    child: MaterialButton(
-                      onPressed: () {},
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        crossAxisAlignment:
-                        CrossAxisAlignment.center,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.end,
                         children: [
-                          Icon(
-                            Icons.play_arrow_rounded,
-                            color: cBackGroundWhite,
-                            size: 18.sp,
-                          ),
-                          SizedBox(
-                            width: 5.w,
-                          ),
-                          CustomTextView(
-                            text: "Play",
-                            fontColor: const Color(0xffFFFFFF),
-                            fontSize: 14.sp,
+                          Flexible(
+                            child: CachedNetworkImage(
+                              height: 100.w,
+                              fit: BoxFit.cover,
+                              imageUrl:
+                              //widget.yogaListVO.videoImage ?? "",
+                              "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS5-F8otEAn1mYIv9mvLPu97JHBMG_9IWR_MVhxjktJ7SRe-BwAAxUC2I-HjgY-jGbwVSM&usqp=CAU",
+                              progressIndicatorBuilder:
+                                  (context, url, downloadProgress) =>
+                                  Center(
+                                    child: CircularProgressIndicator(
+                                        value: downloadProgress.progress),
+                                  ),
+                              errorWidget: (context, url, error) =>
+                              const Icon(Icons.error),
+                            ),
                           ),
                         ],
-                      ),
-                    ),
-                  ),
-                ],
+                      )),
+                ),
               ),
-              // Flexible(
-              //     child: HomePageListview()
-              // )
-              Container(color: cSecondaryOne,
-              padding: EdgeInsets.symmetric(horizontal: 10,vertical: 10),
-              )
-            ],
-          ),
+            ];
+          },
+          body:Container(
+            height: 30,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.only(topRight: Radius.circular(20),topLeft: Radius.circular(20)),
+              color: Colors.white
+            ),
+          )
         ),
       ),
     );
