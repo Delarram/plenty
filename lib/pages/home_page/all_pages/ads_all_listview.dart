2db8d85c24cd335fb65cf5a28b9b504d56f81c70
Delarram <tunyanaing960@gmail.com>
2023-03-24 00:36:12 +0630

    adspage

--- lib/pages/home_page/all_pages/ads_all_listview.dart
+++ lib/pages/home_page/all_pages/ads_all_listview.dart
@@ -1,32 +1,34 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class AdsListItem extends StatelessWidget {
   const AdsListItem({
-    Key? key,
-    required this.adsList,
+    Key? key, required this.imageList,
+
   }) : super(key: key);
 
-
+final String imageList;
 
   @override
   Widget build(BuildContext context) {
     return ListView.separated(
       physics: const NeverScrollableScrollPhysics(),
       shrinkWrap: true,
-      itemCount: adsList.length,
+      itemCount: imageList.length,
       separatorBuilder: (BuildContext context, int index) {
-        return SizedBox(
-          height: 10.h,
+        return const SizedBox(
+          height: 10,
         );
       },
-      itemBuilder: (context, index) => Container(
-        height: 140.h,
+      itemBuilder: (context, index) => SizedBox(
+        height: 140,
         width: double.infinity,
         child: ClipRRect(
-          borderRadius: BorderRadius.circular(13.r),
+          borderRadius: BorderRadius.circular(13),
           child: CachedNetworkImage(
             fit: BoxFit.fill,
-            imageUrl: "",
+            imageUrl: imageList,
             progressIndicatorBuilder: (context, url, downloadProgress) =>
                 Center(
                   child:
--- lib/pages/home_page/all_pages/ads_all_page.dart
+++ lib/pages/home_page/all_pages/ads_all_page.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:newpj/bloc/ads_list_bloc.dart';
+import 'package:newpj/widgets/custom_text.dart';
+import 'package:provider/provider.dart';
+
+import 'ads_all_listview.dart';
 
 
 class AdsPage extends StatelessWidget {
@@ -9,16 +14,24 @@ class AdsPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: EdgeInsets.symmetric(vertical: 10.h, horizontal: 10.w),
-          child: AdsListItem(
-              adsList:bloc.adsList
-          ),
-        ),
+    return ChangeNotifierProvider(
+      create: (context) => AdsListBloc(),
+      child: Consumer<AdsListBloc>(
+        builder: (context, bloc, child) {
+          return Scaffold(
+            appBar:  AppBar(
+              title: CustomTextView(text: "Local Ads",),
+            ),
+            body: SingleChildScrollView(
+              child: Padding(
+                padding: EdgeInsets.symmetric(vertical: 10.h, horizontal: 10.w),
+                child: AdsListItem(imageList: bloc.images.toString()??"",
+                ),
+              ),
+            ),
+          );
+        },
       ),
     );
   }
-
 }
